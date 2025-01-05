bf82492de779d342b7d03af0267475a0b378d4b6
tesfaTMH <hagos.tesfam@gmail.com>
2025-01-05 07:14:51 +0200

    Add custom not-found page

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unable to find the requested resources</p>
+    </main>
+  );
+};
+
+export default NotFound;
