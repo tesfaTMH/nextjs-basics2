a32fe355f7dd0e6ec22ca2ebad0d30d7310c5661
tesfaTMH <hagos.tesfam@gmail.com>
2025-01-05 03:12:51 +0200

    Add custom loading functionality to your app

---
+++ app/loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./loadingMeals.module.css";
+
+const LoadingMeals = () => {
+  return <p className={classes.loading}>Featching meals...</p>;
+};
+
+export default LoadingMeals;
