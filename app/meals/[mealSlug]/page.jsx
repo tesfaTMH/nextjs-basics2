52efa35718220ef144c36d1ff6e27edb035aaade
tesfaTMH <hagos.tesfam@gmail.com>
2025-01-05 10:31:49 +0200

    Update the dynamic meal sulg page and cretae a custom not-found for the meal route

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-const MealByIdPage = () => {
-  return <div>MealByIdPage</div>;
+const MealByIdPage = async ({ params }) => {
+  const meal = await getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
 export default MealByIdPage;
---
+++ app/meals/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal Not found</h1>
+      <p>Unable to find the requested meal data resources</p>
+    </main>
+  );
+};
+
+export default NotFound;
